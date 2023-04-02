131dc4d17f13fbf26a931ec54666d621c0c4d3e6
aimbjp <aimbjp@gmail.com>
2023-04-02 23:46:13 +0300

    initial commit

---
+++ FormalConceptAnalysis/FormalContext.cs
@@ -0,0 +1,19 @@
+﻿namespace FormalConceptAnalysis;
+
+public class FormalContext
+{
+    public static void InputFormalContext()
+    {
+        throw new NotImplementedException();
+    }
+
+    public static void ShowDiagramFormalContext()
+    {
+        throw new NotImplementedException();
+    }
+
+    public static void OutputFormalContext()
+    {
+        throw new NotImplementedException();
+    }
+}
---
+++ FormalConceptAnalysis/Program.cs
@@ -0,0 +1,48 @@
+﻿
+
+namespace FormalConceptAnalysis
+{
+    internal class Program
+    {
+        private static bool _flagStopExecution = false;
+        /// <summary>
+        /// Enterence in the program
+        /// main communication with the user
+        /// Menu; Info; Input; Output;
+        /// </summary>
+        private static void Main(string[] args)
+        {
+            while (!_flagStopExecution)
+            {
+                ShowMenu();
+                ShowInfo();
+                switch (char.ToLower(Console.ReadKey(true).KeyChar))
+                {
+                    case '1': FormalContext.InputFormalContext(); break;
+                    case '2': FormalContext.ShowDiagramFormalContext(); break;
+                    case '3': FormalContext.OutputFormalContext(); break;
+                    case 'e': _flagStopExecution = true; break;
+                }
+            }
+        }
+        /// <summary>
+        /// Show information about the program
+        /// </summary>
+        private static void ShowInfo()
+        {
+            Console.WriteLine("SOME INFORMATION");
+        }
+        /// <summary>
+        /// Show main menu of the program
+        /// </summary>
+        private static void ShowMenu()
+        {
+            Console.Clear();
+            Console.WriteLine("Main menu of the program\n" +
+                              "1) Input formal context; input 1\n" +
+                              "2) Show diagram; input 2\n" +
+                              "3) show formal context; input 3\n" +
+                              "4) exit; input e\n");
+        }
+    }
+}
